26bd2168782769b21be75a40c19f84524707d30c
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2023-05-24 00:48:55 -0700

    Add project files.

---
+++ WebApplication4/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/Welcome/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return HtmlEncoder.Default.Encode($"Hello Tram, NumTimes is: 4");
+        }
+    }
+}
---
+++ WebApplication4/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+namespace MvcMovie.Controllers
+
+{
+
+
+    public class HelloWorldController1 : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
